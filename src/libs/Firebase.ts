d93bb810cabaa272af829769ad3dd992c308784e
Ryo Nishimura <niharu.dev@gmail.com>
2023-02-01 19:21:54 +0900

    secretsの指定が間違っているので修正

--- src/libs/Firebase.ts
+++ src/libs/Firebase.ts
@@ -9,6 +9,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
