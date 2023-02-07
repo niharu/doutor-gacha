70655088873acb820310c33961d6d373e1b76d7d
Ryo Nishimura <wv.nsmr@gmail.com>
2023-02-07 17:34:18 +0900

    refactor: 不要なコメントアウトを削除

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -5,19 +5,12 @@ const theme = extendTheme({
     global: {
       body: {
         backgroundColor: "#fbd24d",
-        // backgroundColor: "yello",
-        // color: "gray.800",
       },
       p: {
         fontSize: { base: "md", md: "lg" },
         lineHeight: "tall",
       },
     },
-  },
-  fonts: {
-    // body: `"メイリオ"`,
-    // heading: `cursive,"Hiragino Kaku Gothic ProN"`,
-    // body: `cursive,"Hiragino Kaku Gothic ProN"`,
   },
 });
 
